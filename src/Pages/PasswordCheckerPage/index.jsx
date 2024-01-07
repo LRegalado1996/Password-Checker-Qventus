1e9ec51b7d114ecded88295f60a710d14be9fefd
Lucas Regalado <lucasregalado@hotmail.com>
2024-01-07 13:48:51 -0300

    Create PasswordChecker Page

---
+++ src/Pages/PasswordCheckerPage/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.scss";
+
+const PasswordCheckerPage = () => {
+  return (
+    <div className="PasswordCheckerPage">
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default PasswordCheckerPage;
---
+++ src/Pages/index.jsx
@@ -0,0 +1,2 @@
+
+export { default as PasswordCheckerPage } from './PasswordCheckerPage';
