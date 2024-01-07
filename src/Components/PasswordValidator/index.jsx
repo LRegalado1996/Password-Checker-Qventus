6a9744368bcd33028b5238e5c845124f252cfbc5
Lucas Regalado <lucasregalado@hotmail.com>
2024-01-07 19:43:02 -0300

    add all check password functions

--- src/Components/PasswordValidator/index.jsx
+++ src/Components/PasswordValidator/index.jsx
@@ -23,21 +23,65 @@ const defaultValidators = {
   noConsecutiveLetters: {
     key: "noConsecutiveLetters",
     text: "Consecutive letters",
-    valid: false,
+    valid: true,
     show: true,
   },
 };
 
-//const specialCharacters = "!@#$%^&*";
+const specialCharactersRegex = /[!@#$%^&*]/;
+const numberRegex = /[0-9]/;
+const uppercaseLetterRegex = /[A-Z]/;
+const consecutiveLettersRegex = /[a-zA-Z]{2}/;
 
 const PasswordValidator = ({ value, options }) => {
-  const { specialCharacters, number, uppercaseLetter, noConsecutiveLetters } =
-    options;
-
   const [validators, setValidators] = useState({});
 
+  const checkPassword = () => {
+    let newValidators = { ...validators };
+
+    for (const item in validators) {
+      const validator = validators[item];
+      if (validator.show) {
+        switch (validator.key) {
+          case "specialCharacters":
+            newValidators.specialCharacters = {
+              ...newValidators.specialCharacters,
+              valid: specialCharactersRegex.test(value),
+            };
+            break;
+
+          case "number":
+            newValidators.number = {
+              ...newValidators.number,
+              valid: numberRegex.test(value),
+            };
+            break;
+
+          case "uppercaseLetter":
+            newValidators.uppercaseLetter = {
+              ...newValidators.uppercaseLetter,
+              valid: uppercaseLetterRegex.test(value),
+            };
+            break;
+
+          case "noConsecutiveLetters":
+            newValidators.noConsecutiveLetters = {
+              ...newValidators.noConsecutiveLetters,
+              valid: !consecutiveLettersRegex.test(value),
+            };
+            break;
+
+          default:
+            break;
+        }
+      }
+    }
+
+    setValidators(newValidators);
+  };
+
   useEffect(() => {
-    let newDefaultValidators = defaultValidators;
+    let newDefaultValidators = { ...defaultValidators };
     for (const key in defaultValidators) {
       newDefaultValidators[key] = {
         ...defaultValidators[key],
@@ -49,18 +93,9 @@ const PasswordValidator = ({ value, options }) => {
   }, [options]);
 
   useEffect(() => {
-    if (specialCharacters) checkSpecialCharacters();
-    if (number) checkNumber();
-    if (uppercaseLetter) checkUppercaseLetter();
-    if (noConsecutiveLetters) checkNoConsecutiveLetters();
-
-    //console.log(validators);
-  }, [value, specialCharacters, number, uppercaseLetter, noConsecutiveLetters]);
-
-  const checkSpecialCharacters = () => {};
-  const checkNumber = () => {};
-  const checkUppercaseLetter = () => {};
-  const checkNoConsecutiveLetters = () => {};
+    if (Object.keys(validators).length > 0) checkPassword();
+  }, [value]);
+  
 
   const allValidators = () => {
     const result = [];
